0ce08a006a8ee616ee0e391dffea2f167a4c93de
Ubuntu <ubuntu@ip-172-31-82-235.ec2.internal>
2019-07-30 15:55:32 +0000

    trouble with rendering addtask

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://ananya:<PASSWORD>@myfirstcluster-nesmd.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'mongodb+srv://ananya:PASSWORD@myfirstcluster-nesmd.mongodb.net/task_manager?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
@@ -15,6 +15,9 @@ mongo = PyMongo(app)
 def hello():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
